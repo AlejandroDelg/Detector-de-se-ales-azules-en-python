022f8c12be941d0af9955ce47cd408be2ee76519
AlejandroDelg <alexdelga2013@gmail.com>
2023-05-02 16:31:18 +0200

    Actualizado anotaciones y JSON

--- cargarJSON.py
+++ cargarJSON.py
@@ -6,7 +6,7 @@ with open('anotaciones/Anotaciones.json', 'r') as f:
 
 
 # Iterar sobre las imágenes
-for imagen_id, imagen_data in data['_via_img_metadata'].items():
+for imagen_id, imagen_data in data.items():
     print(f'Imagen: {imagen_data["filename"]}')
 
     # Iterar sobre las regiones
@@ -14,3 +14,5 @@ for imagen_id, imagen_data in data['_via_img_metadata'].items():
         shape_attributes = region['shape_attributes']
         print(f'Posición x: {shape_attributes["x"]}')
         print(f'Posición y: {shape_attributes["y"]}')
+        print(f'height : {shape_attributes["height"]}')
+        print(f'width : {shape_attributes["width"]}')
